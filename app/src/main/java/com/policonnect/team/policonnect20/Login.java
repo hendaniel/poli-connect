e4e6098f136a1df7991e4d0b9e694c433d3a613d
Andrés Osorio <36467648+GDAndres98@users.noreply.github.com>
2018-09-11 00:37:20 -0500

    Revert "Actualizacion del diseño XML"

--- app/src/main/java/com/policonnect/team/policonnect20/Login.java
+++ app/src/main/java/com/policonnect/team/policonnect20/Login.java
@@ -2,7 +2,6 @@ package com.policonnect.team.policonnect20;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
 
@@ -11,6 +10,4 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-
-    private EditText test;
 }
