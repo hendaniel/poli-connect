34ffb381198b9401a5dd8278bd20c27fa4f24d49
Unknown <edarevalo01@gmail.com>
2018-09-12 15:24:58 -0500

    Pantalla de Usuario preliminar
    Inclusión de datos de usuario en el encabezado de la pantalla.
    (Tener en cuenta que estos datos serán importados de la base de datos y los que están en este momento son preliminares)

---
+++ app/src/main/java/com/policonnect/team/policonnect20/Usuario.java
@@ -0,0 +1,18 @@
+package com.policonnect.team.policonnect20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class Usuario extends AppCompatActivity {
+
+    //private TextView nombreUsuario;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usuario);
+        //nombreUsuario = (TextView)findViewById(R.id.nombreUsuario);
+        //nombreUsuario.setText("Alejandro Arevalo");
+    }
+}
