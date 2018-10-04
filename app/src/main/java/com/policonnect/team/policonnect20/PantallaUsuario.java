cfd186a539d0a0cac0ebaa2dbf035869cd735f8e
Andrés <gd_andres98@hotmail.com>
2018-10-04 01:33:46 -0500

    Tengo sueño

---
+++ app/src/main/java/com/policonnect/team/policonnect20/PantallaUsuario.java
@@ -0,0 +1,26 @@
+package com.policonnect.team.policonnect20;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+/**
+ * @version 1
+ * @author: PoliConnect Team
+ */
+public class PantallaUsuario extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pantalla_usuario, container, false);
+        return view;
+    }
+}
