81fc0d10698ec8c3fa8b905578e881e11c0dc143
Andrés <gd_andres98@hotmail.com>
2018-11-09 18:46:12 -0500

    Actividades menos gimnasio

---
+++ app/src/main/java/com/policonnect/team/policonnect20/Objects/DataBienestar.java
@@ -0,0 +1,59 @@
+package com.policonnect.team.policonnect20.Objects;
+
+import java.util.ArrayList;
+import java.util.TreeSet;
+
+public class DataBienestar {
+
+    private String name;
+    private String teacher;
+    private String place;
+    private TreeSet<Integer>[] horario;
+
+    public DataBienestar(String name, String teacher, String place) {
+        this.name = name;
+        this.teacher = teacher;
+        this.place = place;
+        horario = new TreeSet[6];
+        for (int i = 0; i < horario.length; i++)
+            horario[i] = new TreeSet<Integer>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public String getPlace(){
+        return place;
+    }
+
+    /**
+     * Agrega un bloque al dia seleccionado
+     *
+     * @param time Identifica la hora a la que se ve la actividad en el dia day siendo 1
+     *             de 7:00 a 8:30, 2 de 8:40 a 10:10, 3 de 10:20 a 11:50, 4 de 12:00 a 13:30, 5 de
+     *             13:40 a 15:10, 6 de 15:20 a 16:50, 7 de 17:00 a 18:30, 8 de 18:40 a 20:10, 9 de
+     *             20:20 a 21:50.
+     * @param day  Dia en el que se ve la actividad siendo 0 Lunes, 1 Martes, 2 Miércoles, 3 Jueves,
+     *             4 Viernes y 5 Sábado.
+     * @return true si el bloque ya estaba agregado, falso sino
+     */
+    public boolean addTimeToDay(int time, int day) {
+        return horario[day].add(time);
+    }
+
+    /**
+     * @param day Dia en el que se ve la actividad siendo 0 Lunes, 1 Martes, 2 Miércoles, 3 Jueves,
+     *            4 Viernes y 5 Sábado.
+     * @return Set conteniendo en horario
+     */
+    public TreeSet<Integer> getDayTimes(int day) {
+        return horario[day];
+    }
+
+
+}
--- app/src/main/java/com/policonnect/team/policonnect20/WelfareScreens/BienestarPintura.java
+++ app/src/main/java/com/policonnect/team/policonnect20/WelfareScreens/BienestarPintura.java
@@ -2,14 +2,58 @@ package com.policonnect.team.policonnect20.WelfareScreens;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.policonnect.team.policonnect20.DataBase;
+import com.policonnect.team.policonnect20.Objects.DataBienestar;
 import com.policonnect.team.policonnect20.R;
 
 public class BienestarPintura extends Activity {
 
+    private ImageButton mBackButton;
+    private ImageView mHeaderColor;
+    private TextView mNameScreen;
+    private TextView mNameScreen2;
+    private TextView mPlace;
+    private TextView mProfessor;
+    private TextView mSchedule;
+    private DataBienestar datos;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dummy);
+        setContentView(R.layout.activity_bienestar);
+
+        setViews();
+
+        datos = DataBase.getWelfareActivity(5);
+
+        mHeaderColor.setColorFilter(R.color.colorPintura);
+        mNameScreen.setText(datos.getName());
+        mNameScreen2.setText(datos.getName());
+        mPlace.setText(datos.getPlace());
+        mProfessor.setText(datos.getTeacher());
+
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+    }
+
+    private void setViews() {
+        mBackButton = findViewById(R.id.buttonBackBienestar);
+        mHeaderColor = findViewById(R.id.headerColor);
+        mNameScreen = findViewById(R.id.nameScreen);
+        mNameScreen2 = findViewById(R.id.nameScreen2);
+        mPlace = findViewById(R.id.place);
+        mProfessor = findViewById(R.id.professor);
+        mSchedule = findViewById(R.id.schedule);
     }
 }
